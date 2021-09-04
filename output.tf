ebdfd9e86f5064bb92e2d871522ec489cbbcacfa
sathyanarayanan.sk <sksathya2810@gmail.com>
2021-09-04 15:45:51 +0530

    output.tf

---
+++ output.tf
@@ -0,0 +1,24 @@
+output "alb_name" {
+  value = aws_alb.main.name
+}
+
+output "alb_arn" {
+  value = aws_alb.main.arn
+}
+
+output "dns_name" {
+  value = aws_alb.main.dns_name
+}
+
+output "alb_arn_suffix" {
+  value = aws_alb.main.arn_suffix
+}
+
+output "alb_zone_id" {
+  value = aws_alb.main.zone_id
+}
+
+output "id" {
+  value = aws_alb.main.id
+}
+
