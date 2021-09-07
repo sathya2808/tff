7874bf4f9fc6f2c525d7bb35559171b3742f7aa8
sathyanarayanan.sk <sksathya2810@gmail.com>
2021-09-07 13:45:23 +0530

    script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,13 @@
-git init
 a=$(ls)
-for i in $a; do
-	git add $i
-	git commit -m "$i"
+c=1
+for i in $a;do
+ git add $i
+ if [[($c%4==0)]];
+ then
+   git commit -m "$i"
+   git branch -M main
+   git remote add origin https://github.com/sathya2808/commitdemo.git
+   git push origin main
+ fi
+ c=$c+1
 done
-git branch -M main
-git remote add origin https://github.com/sathya2808/tff.git 
-git push origin main
